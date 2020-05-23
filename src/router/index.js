c30dca2196c2527e8bac1a596d97e067692c0754
Brian <1204194925@qq.com>
2020-05-23 15:28:21 +0800

    router completed

--- src/router/index.js
+++ src/router/index.js
@@ -13,10 +13,22 @@ Vue.use(VueRouter)
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/calculate',
+    name: 'Calculate',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Calculate.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "about" */ '../views/login_text.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
   }
 ]
 
